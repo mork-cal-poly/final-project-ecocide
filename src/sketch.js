c434634481fb11f7e059b03bb99e913c503b8648
Tegan Martin <tmarti80@calpoly.edu>
2024-03-13 08:24:19 -0700

    added trash

--- src/sketch.js
+++ src/sketch.js
@@ -13,6 +13,26 @@ function draw() {
     text("Hi!", 200, 200);
   }
   timer++;
+  
+  drawTrash(200, 200, 1.2);
+
 }
 
+function drawTrash(x, y, s) {
+  push();
+  scale(s);
+  translate(x, y);
+  stroke(80);
+  strokeWeight(1);
+  fill(155, 130, 130);
+  quad(55, 46, 64, 57, 33, 70, 26, 45)
+  fill(150, 120, 80);
+  quad(44, 45, 65, 60, 40, 70, 24, 63)
+  fill(190, 150, 120);
+  quad(50, 50, 60, 70, 40, 70, 30, 55)
+  fill(150, 150, 100);
+  quad(40, 60, 55, 70, 40, 65, 30, 65)
+  
+  pop(); 
+}
 
